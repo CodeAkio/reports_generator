dc3792a57d07158019e6f913d5d415b504e7d326
CodeAkio <siqueira_2004@hotmail.com>
2021-09-20 18:31:57 -0300

    Total cost by user

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,5 +1,15 @@
 defmodule ReportsGenerator do
-  def hello do
-    :world
+  alias ReportsGenerator.Parser
+
+  def build(filename) do
+    filename
+    |> Parser.parse_file()
+    |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
   end
+
+  defp sum_values([id, _food_name, price], report) do
+    Map.put(report, id, report[id] + price)
+  end
+
+  defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 end
